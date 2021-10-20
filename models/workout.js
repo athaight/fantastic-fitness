d69f0b6a6c376ea573e399d6bf344538aea4aa85
Andrew Haight <haight.haight@gmail.com>
2021-10-19 18:55:59 -0700

    rebuild schema with annas help

--- models/workout.js
+++ models/workout.js
@@ -2,43 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
- date: {
-        type: Date,
-        default: () => new Date()
+    day:{
+       type: Date,
+       default: () => new Date()
     },
-  exercises: [
-      {
-    type: {
-        type: String,
-        required: "Type required"
-    },
-    name: {
-        type: String,
-        trim: true,
-        required: "Name required"
-    },
-    weight: {
-        type: Number,
-    },
-    sets: {
-        type: Number,
-    },       
-    reps: {
-        type: Number,
-    },
-    duration: {
-        type: Number,
-        required: "Duration required"
-    },
-    },
-]
-});
-
-// workoutSchema.methods.setTotalDuration = function () {
-//     return this.exercises.reduce((total, exercise) => {
-//         return total + exercise.duration;
-//       }, 0);
-//     };
+    exercises:[
+       {
+          type:{
+             type:String,
+             required: " Enter a Workout Type!"
+          },
+          name:{
+             type: String,
+             required: "Enter a Exercise Name!"
+          },
+          duration:{
+             type:Number,
+          },
+          weight:{
+             type: Number
+          },
+          reps:{
+             type: Number
+          },
+          sets: {
+             type: Number
+          }, 
+          distance:{
+             type: Number
+          }
+       }
+    ]
+ });
     
 const Workout = mongoose.model("Workout", workoutSchema);
 
