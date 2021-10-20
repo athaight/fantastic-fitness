c0dfb5287b80ec1e6ed297cca369dd8fadb26ba0
Andrew Haight <haight.haight@gmail.com>
2021-10-19 18:58:28 -0700

    trying aggregate method per annas help

--- routes/workout.js
+++ routes/workout.js
@@ -1,51 +1,56 @@
-const express = require("express");
-const router = express.Router();
-const Workout = require("../models");
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
-router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .then(workout_db => {
-      res.json(workout_db);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create({})
-    console.log(body)
-      .then(workout_db => {
-        res.json(workout_db);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+router.get("/api/workouts/", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.post("/api/workouts/", (req, res) => {
+  Workout.create({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.json(err));
+});
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
     { $push: { exercises: body } },
-
     { new: true, runValidators: true }
   )
-    .then(workout_db => {
-      res.json(workout_db);
+    .then((data) => {
+      res.json(data);
     })
-    .catch(err => {
-      res.json(err);
-    });
+    .catch((err) => res.json(err));
 });
 
-router.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-      .then(workout_db => {
-        res.json(workout_db);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-
-  module.exports = router;
+module.exports = router;
